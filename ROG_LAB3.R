4e2a002c5f8484a80530621fb2cce4d3a3d337d6
Rodrigo Ortega García <rodrigo.dcea@gmail.com>
2020-08-18 19:22:03 -0500

    Update ROG_LAB3.R

--- ROG_LAB3.R
+++ ROG_LAB3.R
@@ -1,3 +1,19 @@
+# Hecho con gusto por Rodrigo Ortega García
+
+# T1_03_ESPACIO PRODUCTO(2) - RELACIONAMIENTO "CASO REAL": EXPORTACIONES HIDALGUENSES
+
+# Objetivo: Estimar el relacionamiento (relatedness, proximidad, métrica de distancia entre productos)
+# ------------------------------------------------------------------------------------------------------
+# En este ejercicio vamos a:
+# 1. Cargar nuestra matriz de datos
+# 2. Calcular co ocurrencias entre lugares y productos 
+# 3. Estimar el relacionamiento, teniendo como input las co ocurrencias
+# 4. Graficar
+# 5. Exportar los resultados para trabajarlos con cytoscape o gephi
+
+#######################################
+# practica 2: exportaciones hidalguenses #
+#######################################
 library(EconGeo)
 M = as.matrix(
   read.csv("C:/Users/turismo/Dropbox/DCEA/4to. Semestre/Ciencia de datos/Documentos para laboratorios/LAB3/rca_2014.csv" , 
@@ -12,6 +28,11 @@ C=co.occurrence(t(M))
 # co-ocurrencia entre productos t es transpuesta
 C
 r
+# estima el relacionamiento o proximidad pero normalizado, para asegurar que el numero de co ocurrencias que observamos
+# es mayor al numero de co ocurrencias probables (probailidad condicional)
+#tie and Surfboard puede que no esten tan relacionados, abajo de  1 no es relacionado mientras que mayor a 1 si es relacionado
+
+#opcional (sólo para trabajar con datos binarios)
 r[r<1] = 0
 r[r>1] = 1
 r
